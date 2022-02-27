09a384b87b764adc76883127dd3b7409a24b89f0
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-02-28 00:20:31 +0100

    add dbz module

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+  personajes: Personaje[] = [
+    {
+      nombre:'Goku',
+      poder: 15000
+    },
+    {
+      nombre:'Vegeta',
+      poder: 7500
+    }
+  ]
+  nuevo: Personaje = {
+    nombre:'Trunks',
+    poder: 14000
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length == 0) {
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
+    console.log(this.nuevo);
+  }
+
+}
